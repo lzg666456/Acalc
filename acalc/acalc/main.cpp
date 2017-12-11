3e102db5e4d4fff0cb84049ddd2ac596ab208d8e
lzg666456 <lzg666456@163.com>
2017-12-11 09:18:50 +0800

    first commit

---
+++ acalc/acalc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  acalc
+//
+//  Created by admin on 2017/9/8.
+//  Copyright © 2017年 lzg666456. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
